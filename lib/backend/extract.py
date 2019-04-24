df1790cfa2f731f6f5c5f88d22ffdca8e52262ed
King Xia <kxhere@gmail.com>
2019-04-24 04:26:16 -0400

    Further specify journal search
    Journal cites now only return when in the form of ", Harv. J.L. &
    Tech.," to clamp down on extraneous cites. This will not remove them
    completely, but will hit a lot. This is untested so may break code.

--- lib/backend/extract.py
+++ lib/backend/extract.py
@@ -3,9 +3,10 @@ import re
 
 ## TODO: add ", %s ," around all journal names for citations, to avoid things like Transactions being cited everywhere.
 ## TODO: check for journal misspellings?
+## TODO: add check for spacing errors with \s+
 def all_journals_regex():
     return '(%s)' % (
-        '|'.join(map(lambda x: JOURNALS[x].short.replace('.', '\\.'), range(len(JOURNALS)))))
+        '|'.join(map(lambda x: ', %s,' % JOURNALS[x].short.replace('.', '\\.'), range(len(JOURNALS)))))
 
 def extract_cites(case):
     cites = []
@@ -14,7 +15,7 @@ def extract_cites(case):
         return cites
 
     for i in JOURNALS:
-        j_cites = [m.start() for m in re.finditer(JOURNALS[i].short, case['casebody']['data'])]
+        j_cites = [m.start() for m in re.finditer(', %s,' % JOURNALS[i].short, case['casebody']['data'])]
         for cite in j_cites:
             cites.append((i, cite))
 
